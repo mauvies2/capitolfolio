0b08a27bec26290dd6e41d95e68cc94119cf7482
Mauricio Viera <mauricio.viera@trioteca.com>
2021-04-27 23:10:35 +0200

    base app/database set up and config

---
+++ api/src/database.ts
@@ -0,0 +1,18 @@
+import mongoose, { ConnectOptions } from 'mongoose'
+import config from './config'
+
+
+(async () => {
+    try {
+        const mongooseOptions: ConnectOptions = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            // user: config.MONGO_USER,
+            // pass: config.MONGO_PASSWORD
+        }
+        const db = await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`, mongooseOptions)
+        console.log('Database is connected to:', db.connection.name)
+    } catch(error){
+        console.log(error)
+    }
+})()
