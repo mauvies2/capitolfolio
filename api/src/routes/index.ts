4fccfcf84022211f338402daeec418254b21f799
Mauricio Viera <mauricio.viera@trioteca.com>
2021-05-08 00:19:50 +0200

    adds users and dashboard routes

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
+
 import savingsRoutes from '../components/savings/savings.routes'
+import usersRoutes from '../components/users/users.routes'
+import dashboardRoutes from '../components/dashboard/dashboard.routes'
 
 const router = Router()
 
-router.get('/', (req, res) => res.json('home'))
-
 router.use('/savings', savingsRoutes)
+router.use('/users', usersRoutes)
+router.use('/dashboard', dashboardRoutes)
 
 export default router
